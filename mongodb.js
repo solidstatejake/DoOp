0e12d239693e111cec1f9d5eaf5b3ed06aa1c22d
solidstatejake <jberg3@binghamton.edu>
2019-07-01 14:44:18 -0400

    Integrate mongodb into project. Install mongoose.

--- mongodb.js
+++ mongodb.js
@@ -1,12 +1,35 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 const log = console.log;
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'do-op';
 
-MongoClient.connect(connectionURL, { useNewURLParser: true}, (error, client) => {
-  if(error) { return log("ERROR"); }
+const id = new ObjectID();
 
-  log('Database connection successful.');
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+
+  // Handle database connection
+  if (error) return log("ERROR! Unable to connect to database.\n", error);
+  log("Connection to database ", databaseName, " successful.");
+
+  // Begin database handling
+
+  const db = client.db(databaseName);
+
+  db.collection('users').find({ age: 27}).toArray((error, users) => {
+    const jake = users[0];
+    db.collection('users').updateOne({
+      _id: jake._id
+    }, {
+      $set: {
+        name: 'Alfonse'
+      }
+    }).then((result) => {
+      log(result)
+    }).catch((error) => {
+      log(error)
+    })
+  });
+
 });
+
