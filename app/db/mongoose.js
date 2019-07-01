44d06566550a68bcc03296869e491efee6f38957
solidstatejake <jberg3@binghamton.edu>
2019-07-01 18:21:05 -0400

    Integrate validator package, create User field for email, and add mongoose/validator validations for model input.

--- app/db/mongoose.js
+++ app/db/mongoose.js
@@ -1,5 +1,6 @@
 const log = console.log;
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const connectionURL = 'mongodb://127.0.0.1:27017/do-op-api';
 
@@ -11,27 +12,56 @@ mongoose.connect(connectionURL, {
 const User = mongoose.model('User', {
   name: {
     type: String,
+    required: true,
+    trim: true,
+    minlength: 2,
+    maxlength: 15,
+    validate(name) {
+      const regex = /f+u+c+k+/;
+      if(validator.matches(name, regex)) throw new Error("Name cannot contain" +
+        " profanity.");
+    }
 
   },
+
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(emailAddress){
+      if(!validator.isEmail(emailAddress)){
+        throw new Error('Please enter a valid email address.');
+      }
+    }
+  },
+
   age: {
     type: Number,
+    required: true,
+    min: 13,
+    max: 110
   },
 });
 
 const Task = mongoose.model('Task', {
   description: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
   completed: {
     type: Boolean
   },
 });
 
-const task = new Task({
-  description: 'Walk the doggies!',
-  completed: false
-}).save().then((task) => {
-  log(task)
-}).catch((error) => {
-  log(error)
+
+const katya = new User({
+  name: 'Katya',
+  email:'KiWasiu1@binghamton.edu',
+  age: 21
+}).save().then((user)=>{
+  log(user);
+}).catch((error)=>{
+  log(error);
 });
