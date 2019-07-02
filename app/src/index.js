a7a8f887454f60dbd34c0df51a264b98c10ff80b
solidstatejake <jberg3@binghamton.edu>
2019-07-01 23:04:57 -0400

    Implement PATCH route for user update.

--- app/src/index.js
+++ app/src/index.js
@@ -17,23 +17,23 @@ app.get('/', (req, res) => {
   res.send('Am I up?');
 });
 
-app.get('/users', async (req, res) => {
+app.get('/users', async(req, res) => {
 
   try {
     const users = await User.find({});
     res.send(users);
-  } catch(error){
+  } catch (error) {
     res.status(500).send();
   }
 
 });
 
-app.get('/users/:id', async (req, res) => {
+app.get('/users/:id', async(req, res) => {
   const _id = req.params.id;
 
   try {
     const user = await User.findById(_id);
-    if(!user) return res.status(404).send();
+    if (!user) return res.status(404).send();
     res.send(user);
   }
 
@@ -43,7 +43,7 @@ app.get('/users/:id', async (req, res) => {
 
 });
 
-app.get('/tasks', async (req, res) => {
+app.get('/tasks', async(req, res) => {
 
   try {
     const tasks = await Task.find({});
@@ -56,12 +56,12 @@ app.get('/tasks', async (req, res) => {
 
 });
 
-app.get('/tasks/:id', async (req, res) => {
+app.get('/tasks/:id', async(req, res) => {
   const _id = req.params.id;
 
   try {
     const task = await Task.findById(_id);
-    if(!task) return res.status(404).send();
+    if (!task) return res.status(404).send();
     res.send(task);
   }
 
@@ -73,7 +73,7 @@ app.get('/tasks/:id', async (req, res) => {
 
 // POST
 
-app.post('/users', async (req, res) => {
+app.post('/users', async(req, res) => {
   const user = new User(req.body);
 
   try {
@@ -81,13 +81,13 @@ app.post('/users', async (req, res) => {
     res.status(201).send(user);
   }
 
-  catch(error) {
+  catch (error) {
     res.status(400).send(error);
   }
 
 });
 
-app.post('/tasks', async (req, res) => {
+app.post('/tasks', async(req, res) => {
   const task = new Task(req.body);
 
   try {
@@ -96,11 +96,38 @@ app.post('/tasks', async (req, res) => {
   }
 
   catch (error) {
-    res.status(500).send();
+    res.status(400).send();
   }
 
 });
 
+// PATCH
+
+app.patch('/users/:id', async(req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = [ 'name', 'email', 'age', 'password' ];
+  const isValidUpdate = updates.every((update) => allowedUpdates.includes(update));
+
+  if(!isValidUpdate) {
+    return res.status(400).send({error: "Invalid update field(s) presented."});
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!user) return res.status(404).send();
+
+    res.send(user);
+  }
+
+  catch (error) {
+    res.status(400).send();
+  }
+});
 
 // Listen...
 app.listen(port, () => {
