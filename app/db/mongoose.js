c2a3d0e3a878331abed8776a54c7e0a775bc6da9
solidstatejake <jberg3@binghamton.edu>
2019-07-01 22:35:25 -0400

    Integrate async/await functions for mongoose promises WRT POSTs and GETs for models.

--- app/db/mongoose.js
+++ app/db/mongoose.js
@@ -4,5 +4,6 @@ const connectionURL = 'mongodb://127.0.0.1:27017/do-op-api';
 
 mongoose.connect(connectionURL, {
   useNewUrlParser: true,
-  useCreateIndex: true
+  useCreateIndex: true,
+  useFindAndModify: false
 });
--- app/src/index.js
+++ app/src/index.js
@@ -10,74 +10,95 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Routes
+/* Routes */
+
+// GET
 app.get('/', (req, res) => {
   res.send('Am I up?');
 });
 
-app.get('/users', (req, res) => {
-  User.find({}).then((users) => {
+app.get('/users', async (req, res) => {
+
+  try {
+    const users = await User.find({});
     res.send(users);
-  }).catch((error) => {
+  } catch(error){
     res.status(500).send();
-  });
+  }
+
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
   const _id = req.params.id;
 
-  User.findById(_id).then((user) => {
-    if (!user) {
-      return res.status(404).send();
-    }
-
+  try {
+    const user = await User.findById(_id);
+    if(!user) return res.status(404).send();
     res.send(user);
+  }
 
-  }).catch((error) => {
+  catch (error) {
     res.status(500).send();
-  });
+  }
+
 });
 
-app.get('/tasks', (req, res) => {
-  Task.find({}).then((tasks) => {
+app.get('/tasks', async (req, res) => {
+
+  try {
+    const tasks = await Task.find({});
     res.send(tasks);
-  }).catch((error) => {
+  }
+
+  catch (error) {
     res.status(500).send();
-  });
+  }
+
 });
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const _id = req.params.id;
-  Task.findById(_id).then((task) => {
 
-    if(!task){
-      return res.status(404).send();
-    }
-
+  try {
+    const task = await Task.findById(_id);
+    if(!task) return res.status(404).send();
     res.send(task);
-  }).catch((error) => {
+  }
+
+  catch (error) {
     res.status(500).send();
-  });
+  }
+
 });
 
-app.post('/users', (req, res) => {
+// POST
+
+app.post('/users', async (req, res) => {
   const user = new User(req.body);
 
-  user.save().then(() => {
+  try {
+    await user.save();
     res.status(201).send(user);
-  }).catch((error) => {
+  }
+
+  catch(error) {
     res.status(400).send(error);
-  });
+  }
+
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
 
-  task.save().then(() => {
-    res.status(201).send(task);
-  }).catch((error) => {
-    res.status(400).send(error);
-  });
+  try {
+    await task.save();
+    res.send();
+  }
+
+  catch (error) {
+    res.status(500).send();
+  }
+
 });
 
 
