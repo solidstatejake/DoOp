039894d136122e2b0d08bf3821650dca29fe4cdd
solidstatejake <jberg3@binghamton.edu>
2019-07-02 16:09:52 -0400

    Return user auth token in response for login route.

--- app/routes/user.js
+++ app/routes/user.js
@@ -55,7 +55,7 @@ router.post('/users/login', async(req, res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password);
     const token = await user.generateAuthToken();
-    res.send({ user });
+    res.send({ user, token });
   } catch (error) {
     res.status(400).send(error.message);
   }
