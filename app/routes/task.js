bf5cc4856f4e3cc8a3d97d47fea79819dca9bd7f
solidstatejake <jberg3@binghamton.edu>
2019-07-03 20:56:49 -0400

    Implement query option for task GET route to sort displayed tasks.

--- app/routes/task.js
+++ app/routes/task.js
@@ -3,18 +3,30 @@ const Task = require('../models/Task');
 const auth = require('../middleware/authentication');
 const router = new express.Router();
 
+
+//set sort{ completed: false } to sort by non-completed tasks first
+//
 router.get('/tasks', auth, async(req, res) => {
   const match = {};
+  const sort = {};
+
   if(req.query.completed) {
     match.completed = req.query.completed === 'true'
   }
+
+  if(req.query.sortBy) {
+    const sortQuery = req.query.sortBy.split(':');
+    sort[sortQuery[0]] = sortQuery[1] === 'desc' ? -1 : 1;
+  }
+
   try {
     await req.user.populate({
       path: 'tasks',
       match,
       options: {
         limit: parseInt(req.query.limit),
-        skip: parseInt(req.query.skip)
+        skip: parseInt(req.query.skip),
+        sort
       }
     }).execPopulate();
     res.send(req.user.tasks);
